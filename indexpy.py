4d8c96a58c9a7300093a18b2d41d312e562ffde1
royabhi00 <78011050+royabhi00@users.noreply.github.com>
2021-05-08 11:59:54 +0530

    password generator project

---
+++ indexpy.py
@@ -0,0 +1,7 @@
+import random
+charcter = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y','z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z','@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>','*', '(', ')', '<']
+x = 0
+print("Your password is:")
+for x in range(15):
+    password = random.choice(charcter)
+    print(password)
