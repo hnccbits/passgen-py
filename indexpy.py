5f45af22d3563f69c63ccb591759b9297b5d6764
royabhi00 <78011050+royabhi00@users.noreply.github.com>
2021-05-13 01:18:54 +0530

    Update indexpy.py
    the manual list is removed and CLI tool is included

--- indexpy.py
+++ indexpy.py
@@ -1,7 +1,28 @@
 import random
-charcter = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y','z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z','@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>','*', '(', ')', '<']
-x = 0
-print("Your password is:")
-for x in range(15):
-    password = random.choice(charcter)
-    print(password,end="")
+import argparse
+import sys
+import string
+
+def passgenerator(args):
+    c1 = string.ascii_uppercase
+    c2 = string.ascii_lowercase
+    c3 = string.digits
+    c4 = string.punctuation
+
+    s =[]
+    
+    s.extend(list(c1))
+    s.extend(list(c2))
+    s.extend(list(c3))
+    s.extend(list(c4))
+
+    password = "".join(random.sample(s, args.l))
+    return password
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--l' , type=int, default=15, help ="This is a utility to give a password of the length you want")
+
+    args = parser.parse_args()
+    sys.stdout.write(str(passgenerator(args)))
+
