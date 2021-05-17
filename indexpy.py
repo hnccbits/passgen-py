d8c04c18fce7ce26127cb57c3b2e29be4aea3cc5
Abhishek Anand <78011050+royabhi00@users.noreply.github.com>
2021-05-17 14:19:52 +0530

    Update indexpy.py
    a new update has been done in password generator python program

--- indexpy.py
+++ indexpy.py
@@ -1,6 +1,5 @@
 import random
 import argparse
-import sys
 import string
 import pyperclip
 
@@ -27,7 +26,7 @@ def passgenerator(platform,passlen,lower,upper,digit,special):
         d2 = "".join(random.sample(c2, lower))
         d3 = "".join(random.sample(c3, digit))
         d4 = "".join(random.sample(c4, special))
-    
+
         c.extend(list(d1))
         c.extend(list(d2))
         c.extend(list(d3))
@@ -42,23 +41,32 @@ def passgenerator(platform,passlen,lower,upper,digit,special):
         password = "".join(ps)
 
         pyperclip.copy(password)
-        print(platform," :- ")
-        return password
-    
+        print(platform," :- ",password)
+
     else:
         return "For secure password,Password length should be greater than or eqaul to 15."
 
 
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-pl','--passlen', type = int, help ="password length", default=15)
+parser.add_argument('-w','--platform', type = str, help = "website name", default='Facebook')
+parser.add_argument('-lw','--lower', type = int, help = "Number of lower case characters", default=4)
+parser.add_argument('-up','--upper', type = int, help = "Number of upper case characters", default=3)
+parser.add_argument('-d','--digit', type = int, help = "Number of digits", default=5)
+parser.add_argument('-s','--special', type = int, help = "Number of special characters", default=3)
+
+args = parser.parse_args()
+
 if __name__=='__main__':
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-pl','--passlen', type = int, help ="password length", default=15)
-    parser.add_argument('-w','--platform', type = str, help = "website name", default='Facebook')
-    parser.add_argument('-lw','--lower', type = int, help = "Number of lower case characters", default=4)
-    parser.add_argument('-up','--upper', type = int, help = "Number of upper case characters", default=3)
-    parser.add_argument('-d','--digit', type = int, help = "Number of digits", default=5)
-    parser.add_argument('-s','--special', type = int, help = "Number of special characters", default=3)
+    if args.lower == 4 and args.upper == 3 and args.digit == 5 and args.special == 3:
+        passgenerator(args.platform,args.passlen,args.lower,args.upper,args.digit,args.special)
 
-    args = parser.parse_args()
-    sys.stdout.write(str(passgenerator(args.platform,args.passlen,args.lower,args.upper,args.digit,args.special)))
-    
+    else:
+        if args.passlen == args.lower + args.upper + args.digit + args.special or args.passlen > args.lower + args.upper + args.digit + args.special:
+            passgenerator(args.platform,args.passlen,args.lower,args.upper,args.digit,args.special)
+
+        else:
+            print("password lenght should be greater than or equal to the sum of numbers of the different characters you want")    
