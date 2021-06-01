b6e7258bd85bee39de8b7042df713bb8134b83de
ritumona01 <80118820+ritumona01@users.noreply.github.com>
2021-05-31 21:33:44 -0700

    Add files via upload

---
+++ lib/build/lib/passgen/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+setup(name="passgen",version="0.1",
+description="this is a CLI_TOOL for random password generator",
+author="Ritu",packages=["passgen"],install_requires=[])
