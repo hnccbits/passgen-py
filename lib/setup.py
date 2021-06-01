e13e7ac8b97d08aaec16c2366d8a97d203f8bacc
ritumona01 <80118820+ritumona01@users.noreply.github.com>
2021-06-01 07:48:35 -0700

    Delete setup.py

--- lib/setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup
-setup(name="passgen",version="0.1",
-description="password generator",
-long_description="This is a cli based password generator which generates random password",
-author="Ritu",packages=["passgen"],install_requires=[])
